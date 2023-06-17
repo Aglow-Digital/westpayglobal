c68361ef56738c2feebb2270e32879913d08d28c
stephenchad <stephethanchad@gmail.com>
2023-06-17 23:37:57 +0100

    Update

--- js/test.js
+++ js/test.js
@@ -56,3 +56,29 @@ var swiper = new Swiper(".home-slider", {
     //     prevE1: ".swiper-button-prev",
     // },
 });
+
+
+
+$(document).ready(function() {
+    // HERO SLIDER
+    $('#hero-slider').owlCarousel({
+      loop:true,
+      margin:0,
+      nav:true,
+      items: 1,
+      dots: false,
+      smartSpeed: 1000,
+      navText: ['PREV', 'NEXT'],
+      responsive:{
+          0:{
+        
+          },
+          600:{
+            
+          },
+          1000:{
+            
+          }
+        }
+      })
+    });
